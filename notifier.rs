02b09e492e4298d16ab7b6b0739ec66a1267b7f6
Moti Goldstein <motigoldstin@gmail.com>
2022-12-14 10:00:52 +0200

    Add design
    Signed-off-by: Moti Goldstein <motigoldstin@gmail.com>

---
+++ notifier.rs
@@ -0,0 +1,10 @@
+
+fn main(){
+    // config the request
+    
+    // create thread that sends summary once a day
+    // while true -
+        // perform new request
+        // check if new lottery opened
+        // notify if something change
+}
