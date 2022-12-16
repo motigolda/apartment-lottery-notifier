744ccf788147fc49291b13fb92aa489fa9015442
Moti Goldstein <motigoldstin@gmail.com>
2022-12-16 12:45:29 +0200

    break to files
    Signed-off-by: Moti Goldstein <motigoldstin@gmail.com>

---
+++ main.rs
@@ -0,0 +1,13 @@
+use requests;
+use parser;
+use notifier;
+
+fn main(){
+    // config the request
+    
+    // create thread that sends summary once a day
+    // while true -
+        // perform new request
+        // check if new lottery opened
+        // notify if something change
+}
